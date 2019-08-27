432ae031521e2db48a4863210d767a65b9cf880e
Patricio-Gtz <p.gutierrez.itunes@gmail.com>
2019-08-27 16:51:43 -0500

    Initial Commit

---
+++ Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Patricio Gutierrez on 8/27/19.
+//  Copyright © 2019 Patricio Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
